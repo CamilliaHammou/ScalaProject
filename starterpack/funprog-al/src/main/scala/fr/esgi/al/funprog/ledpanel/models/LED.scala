0bedc5071e4106ac6f5e18f47faf07b29d49753f
3cvms3 <cvms333@yahoo.com>
2025-07-08 10:27:35 +0200

    add class led

--- starterpack/funprog-al/src/main/scala/fr/esgi/al/funprog/ledpanel/models/LED.scala
+++ starterpack/funprog-al/src/main/scala/fr/esgi/al/funprog/ledpanel/models/LED.scala
@@ -1,6 +1,6 @@
 package fr.esgi.al.funprog.ledpanel.models
 
-// Représente une couleur LED avec les composantes RGB (0 ou 1 chacune)
+//ici on represente une couleur LED avec les composantes RGB (0 ou 1 chacune)
 case class Color(red: Int, green: Int, blue: Int) {
   require(red == 0 || red == 1, "Red component must be 0 or 1")
   require(green == 0 || green == 1, "Green component must be 0 or 1")
@@ -26,20 +26,20 @@ object Color {
   val WHITE = Color(1, 1, 1)
 }
 
-// Représente une LED avec sa position, couleur et intensité
+//LED avec sa position, couleur et intensité
 case class LED(x: Int, y: Int, color: Color = Color.BLACK, intensity: Double = 0.0) {
   require(intensity >= 0.0 && intensity <= 1.0, "Intensity must be between 0 and 1")
   
   def isOn: Boolean = intensity > 0.0
   def isOff: Boolean = intensity == 0.0
   
-  // Change l'intensité
+  //ici on va changé l'intensité
   def withIntensity(newIntensity: Double): LED = {
     require(newIntensity >= 0.0 && newIntensity <= 1.0, "Intensity must be between 0 and 1")
     copy(intensity = newIntensity)
   }
   
-  // Change la couleur (seulement si la LED est éteinte)
+  //ici on change la couleur que si la lED est éteinte
   def withColor(newColor: Color): Either[String, LED] = {
     if (isOn && color != newColor) {
       Left("Cannot change color of an LED that is on")
